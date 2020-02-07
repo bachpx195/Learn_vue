c5e39bc1b26a11474083899e57b00eb89ea565a3
bachpx <bach.pham@linkbal.vn>
2020-02-07 14:52:11 +0700

    learn props

---
+++ component-communication/src/components/UserDetail.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="component">
+        <h3>User Details</h3>
+        <p>Tuoi toi: {{ dmm }}</p>
+        <button @click="dmm++">Doi tuoi</button>
+    </div>
+</template>
+
+<script>
+	export default {
+		data: function () {
+			return {
+				dmm: 11
+			}
+		},
+		// props: ['age'],
+		method: {
+			changeAge () {
+				this.dmm = 12
+			}
+		}
+	}
+</script>
---
+++ component-communication/src/components/UserEdit.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="component">
+        <h3>Edit User</h3>
+        <p>Tuoi cua toi: {{ age }}</p>
+        <button>Thay doi</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['age']
+    }
+</script>
